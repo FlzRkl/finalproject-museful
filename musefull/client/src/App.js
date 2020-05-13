c3e2bac4389d6fdfe27d3383f2de37c7d49c9c65
mendritzk.io <hi@mendritzk.io>
2020-05-13 21:55:05 +0200

    new rerun for Musefull UI

---
+++ musefull/client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+//STYLING
+import "./App.scss";
+
+function App() {
+  return (
+    <div className='App'>
+      <h1> Musefull App </h1>
+    </div>
+  );
+}
+
+export default App;
