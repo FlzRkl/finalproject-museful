28c628ea15beb93c6375658aeae159541156bcbc
Staschek <staschek@protonmail.com>
2020-05-28 22:20:56 +0200

    addFunction

--- client/src/components/pages/components/ListItem.jsx
+++ client/src/components/pages/components/ListItem.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ListComponent1 from './ListComponent1';
 
-export default function ListItem() {
+export default function ListItem(lists) {
+  console.log(lists);
   return (
     <div className='col-lg-8'>
       <div>
-        {/* //style={userStyle} */}
-        {list.map((item) => (
-          <ListComponent1 key={item.id} idea={item} />
-        ))}
+        {/* {lists.map((item) => (
+          <ListComponent1 key={item} idea={item} />
+        ))} */}
       </div>
     </div>
   );
