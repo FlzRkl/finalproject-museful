6fdb60d8c7428597b427cfc623c4fa832e2a0c50
Staschek <staschek@protonmail.com>
2020-06-05 23:22:27 +0200

    Listcomponent from useState to useReducer &context

--- client/src/components/pages/ListComponent/ListAtomInput.jsx
+++ client/src/components/pages/ListComponent/ListAtomInput.jsx
@@ -1,21 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import ListItem1 from './ListItem1';
-import ListItem2 from './ListItem2';
+// import ListItem2 from './ListItem2';
+import { ListsContext } from './../../../App';
 
 function ListAtomInput() {
-  const [lists, setLists] = useState([]);
-  const [list, setList] = useState('');
+  const listsContext = useContext(ListsContext);
 
-  const handleAdd = () => {
-    setLists([
-      {
-        id: lists.length,
-        value: list,
-      },
-      ...lists,
-    ]);
-    setList('');
-  };
+  const [list, setList] = useState('');
 
   const handleChange = (e) => {
     setList(e.target.value);
@@ -27,7 +18,11 @@ function ListAtomInput() {
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          handleAdd(e);
+          listsContext.listDispatch({
+            type: 'submit',
+            value: list,
+          });
+          setList('');
         }}
         className='form'
       >
@@ -44,7 +39,18 @@ function ListAtomInput() {
             onChange={handleChange}
           />
           <div className='input-group-append'>
-            <button className='input-group-text' id='basic-addon2'>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                listsContext.listDispatch({
+                  type: 'submit',
+                  value: list,
+                });
+                setList('');
+              }}
+              className='input-group-text'
+              id='basic-addon2'
+            >
               Add
             </button>
           </div>
@@ -53,7 +59,7 @@ function ListAtomInput() {
 
       <hr />
       <div className='col-lg-8'>
-        <ListItem1 lists={lists} />
+        <ListItem1 />
         {/* <ListItem2 lists={lists} /> */}
       </div>
     </>
