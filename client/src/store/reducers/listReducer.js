a4013781c05245701869413a4db8ac7bb1991e83
Staschek <staschek@protonmail.com>
2020-06-16 13:09:13 +0200

    save for switching

--- client/src/store/reducers/listReducer.js
+++ client/src/store/reducers/listReducer.js
@@ -1,48 +1,7 @@
 import { SUBMIT_ITEM, OPEN_ITEM } from '../types';
 
-const initialState = {
-  mainList: {
-    id: uuid(),
-    title: 'Biology',
-    desc: '',
-    date: '',
-  },
-  itemList: [],
-};
+const initialState = { mainList: {}, itemList: [] };
 
 // export default function (state = initialState, action) {
-//   switch (action.type) {
-//     case FETCH_USERS:
-//       return {
-//         ...state,
-//         users: action.payload,
-//       };
-
-//     case SET_LOADING:
-//       return {
-//         ...state,
-//         loading: action.payload,
-//       };
-
-//     case SET_ALERT:
-//       return {
-//         ...state,
-//         alert: action.payload,
-//       };
-
-//     case SET_ALERTBOOL:
-//       return {
-//         ...state,
-//         alertBool: action.payload,
-//       };
-
-//     case SET_TEXT:
-//       return {
-//         ...state,
-//         text: action.payload,
-//       };
-
-//     default:
-//       return state;
-//   }
+//
 // }
