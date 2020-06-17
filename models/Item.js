5bd63fb7c6d1953ec7867aece4dac9a0ae1f9282
Staschek <staschek@protonmail.com>
2020-06-17 12:59:36 +0200

    backend mongodb stuff fiish

---
+++ models/Item.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const ItemSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+  },
+  text: {
+    type: Array,
+  },
+  pic: {
+    type: Array,
+  },
+  video: {
+    type: Array,
+  },
+  list: {
+    type: Array,
+  },
+  link: {
+    type: Array,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Item = mongoose.model('item', ItemSchema);
