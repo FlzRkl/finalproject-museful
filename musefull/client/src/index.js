8916d5b52b903df48d8f1e28a5fad3b2fcf3499a
Staschek <staschek@protonmail.com>
2020-06-17 13:54:53 +0200

    test upload react redux

--- musefull/client/src/index.js
+++ musefull/client/src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 //STYLING
-import "./fontAwesome";
-import "./style/_index.scss";
+import './fontAwesome';
+import './style/_index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
