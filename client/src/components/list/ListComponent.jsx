ef6f2dc6cc7b796dbd622e03c3cea10e033f99c9
Staschek <staschek@protonmail.com>
2020-06-24 23:08:20 +0200

    listComponent li render

--- client/src/components/list/ListComponent.jsx
+++ client/src/components/list/ListComponent.jsx
@@ -1,46 +1,52 @@
-import React, { useState, useEffect, useRef } from 'react';
-import ListItems from './ListItems';
+import React, { useState, useEffect, useRef, Fragment } from 'react';
 import './list.scss';
-import { ItemFilter } from './ItemFilter';
-import { loadItem } from '../../actions/listAction';
-
-import { connect, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
+import ListItems from './ListItems';
+import { ItemFilter } from './ItemFilter';
 import { submitItem } from '../../actions/listAction';
+import { loadItem, getListArr } from '../../actions/listAction';
+import { connect, useSelector, useDispatch } from 'react-redux';
+import { ABOVE_ITEM } from '../../actions/actionTypes';
 
-export const ListComponent = ({ loadItem }) => {
+export const ListComponent = ({ loadItem, getListArr }) => {
   const inputEl = useRef(null);
   const [list, setList] = useState('');
-  const [lists, setLists] = useState([]);
   const mainList = useSelector((state) => state.list.mainList);
-
-  const isArr = (arrList) => {
-    let res = Object.values(mainList);
-    arrList = res.filter((item) => Array.isArray(item));
-    let result = arrList.map((item) => {
-      if (item) {
-        <ListItems subList={item} />;
-      }
-    });
-    console.log(result);
-    return;
-  };
+  const filteredList = useSelector((state) => state.list.filteredList);
+  const [tags, setTags] = useState([]);
+  const [data, setData] = useState([]);
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setList(e.target.value);
   };
 
+  const handleClick = (e) => {
+    const id = e.target.closest('li').id;
+    // dispatch({
+    //   type: ABOVE_ITEM,
+    //   payload: id,
+    // });
+    loadItem(id);
+    console.log(id);
+  };
+
   const submit = (e) => {
     e.preventDefault();
     // submitItem(list);
+    //console.log(lists);
     setList('');
   };
 
   useEffect(() => {
     inputEl.current.focus();
-    loadItem();
-    isArr();
-  }, [loadItem, isArr]);
+    getListArr(mainList);
+  }, [mainList]);
+
+  useEffect(() => {
+    setTags(filteredList[0]);
+    setData(filteredList[1]);
+  }, [filteredList]);
 
   return (
     <>
@@ -71,10 +77,25 @@ export const ListComponent = ({ loadItem }) => {
         </div>
         <ItemFilter />
       </form>
-
       <hr />
       <div className='col-lg-8'>
-        <ul className='list-group list-group-flush'>{<ListItems />}</ul>
+        <ul className='list-group list-group-flush bg-secondary'>
+          {data
+            ? data.map((item, index) => (
+                <>
+                  <h4>{tags[index]}</h4>
+                  <li
+                    key={tags[index] + index}
+                    id={tags[index] + index}
+                    onClick={handleClick}
+                    className='list-group-item'
+                  >
+                    <ListItems item={item} />
+                  </li>
+                </>
+              ))
+            : null}
+        </ul>
       </div>
     </>
   );
@@ -88,4 +109,6 @@ const mapStateToProps = (state) => ({
   list: state.list,
 });
 
-export default connect(mapStateToProps, { loadItem })(ListComponent);
+export default connect(mapStateToProps, { loadItem, getListArr })(
+  ListComponent
+);
