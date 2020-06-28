0e49b0f481ce99bc838e4469226282715baef0fc
mendritzk.io <hi@mendritzk.io>
2020-06-28 22:48:22 +0200

    css added, the hook killed the css variables.

--- musefull/client/src/fontAwesome.js
+++
@@ -1,9 +0,0 @@
-// STYLE
-import { library } from "@fortawesome/fontawesome-svg-core";
-
-import {
-  faCogs,
-  faAngleLeft as faAngleLeftSolid,
-} from "@fortawesome/free-solid-svg-icons";
-
-library.add(faCogs, faAngleLeftSolid);
--- musefull/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://admin:FuLmUsE2020DcI@museful-app-cluster-6fdek.gcp.mongodb.net/museful-app-cluster?retryWrites=true&w=majority",
-};
