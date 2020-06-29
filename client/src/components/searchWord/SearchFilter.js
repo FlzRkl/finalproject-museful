b94c86e87fd5c473e5ea90727b71915c23a33cb9
lanamaxic <lana.maksic@gmail.com>
2020-06-29 21:27:46 +0200

    commit

--- client/src/components/searchWord/SearchFilter.js
+++ client/src/components/searchWord/SearchFilter.js
@@ -1,50 +1,54 @@
-import React from 'react';
-import cx from 'classnames';
+import React from "react";
+import cx from "classnames";
 
-import PropTypes from 'prop-types';
-import { useSelector, useDispatch } from 'react-redux';
-import { SET_SEARCH_FILTER } from '../../actions/actionTypes';
+import PropTypes from "prop-types";
+import { useSelector, useDispatch } from "react-redux";
+import { SET_SEARCH_FILTER } from "../../actions/actionTypes";
 
-import { SEARCH_FILTERS } from './Search_Filters';
+import { SEARCH_FILTERS } from "./Search_Filters";
 
 export const SearchFilter = () => {
   const activeFilter = useSelector((state) => state.search.filter);
   const dispatch = useDispatch();
   return (
     <div>
-      <div className='item-filters form-check form-check-inline'>
+      <div className="item-filters form-check form-check-inline">
+        {" "}
         {Object.keys(SEARCH_FILTERS).map((filterKey) => {
           const currentFilter = SEARCH_FILTERS[filterKey];
+
           return (
             <div
               key={`item-filter-${currentFilter}`}
               className={cx(
                 `item-filter-${currentFilter} form-check form-check-inline`,
-                currentFilter === activeFilter && `active`,
+                currentFilter === activeFilter && `active`
               )}
               onClick={() =>
                 dispatch({
                   type: SET_SEARCH_FILTER,
-                  payload: currentFilter,
+                  payload: filterKey,
                 })
-              }>
+              }
+            >
               <input
-                className='form-check-input'
-                type='radio'
+                className="form-check-input"
+                type="radio"
                 id={`filter-${currentFilter}`}
                 name={`item-filter-radio`}
                 value={`option-${currentFilter}`}
-              />
+              />{" "}
               <label
-                className='form-check-label'
-                htmlFor={`filter-${currentFilter}`}>
-                {' '}
-                {currentFilter}
-              </label>
+                className="form-check-label"
+                htmlFor={`filter-${currentFilter}`}
+              >
+                {" "}
+                {currentFilter}{" "}
+              </label>{" "}
             </div>
           );
-        })}
-      </div>
+        })}{" "}
+      </div>{" "}
     </div>
   );
 };
