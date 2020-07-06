c20afe68c5023c08b48c312cc329759ac8dbb0c7
filizerkal <erkalfiliz@gmx.net>
2020-07-06 18:48:30 +0200

    random

--- client/src/components/dailyLearning/dailyLearning.js
+++ client/src/components/dailyLearning/dailyLearning.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { connect, useSelector } from 'react-redux';
-//import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchRandom } from '../../actions/searchAction';
 
 // STYLE
@@ -9,56 +9,21 @@ import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const dailyLearning = ({ fetchRandom }) => {
-  const [getRandomWord, setRandomWord] = useState('');
-  const isLoading = useSelector((state) => state.search.isLoading);
-  const isError = useSelector((state) => state.search.isError);
-  const data = useSelector((state) => state.search.data);
-  //const filter = useSelector((state) => state.search.filter);
-
-  const handleChange = (e) => {
-    setRandomWord(e.target.value);
+  const getRandomWord = () => {
+    fetchRandom();
   };
 
-  const handleSearch = (e) => {
-    e.preventDefault();
-    fetchWord(getRandomWord);
-  };
   return (
-    <div className='d-flexColumn'>
-      <form id='formSearch' onSubmit={handleSearch}>
-        <input
-          placeholder='Please enter a word..'
-          id='initial-word-form'
-          type='text'
-          value={getRandomWord}
-          onChange={handleChange}
-          className='input'
-          autocomplete='off'
-        />
-        <button
-          onClick={handleSearch}
-          type='submit'
-          className='inputSearch'
-          style={{ marginTop: '8px' }}>
-          Search
-        </button>
-      </form>
-
-      {isError && <div>Something went wrong ...</div>}
-
-      {isLoading ? (
-        <div>Loading ...</div>
-      ) : (
-        <div className='searchResults'>
-          {data
-            ? data.map((item) => (
-                <div key={item.getRandomWord} id={item.getRandomWord}>
-                  <button className='searchResult'>{item.getRandomWord}</button>
-                </div>
-              ))
-            : null}
-        </div>
-      )}
+    <div className='bodyS'>
+      <h1> Daily Learning </h1>{' '}
+      <Link className='' to='/dashboard'>
+        <p className='btnBack'>
+          <FontAwesomeIcon icon={faAngleLeft} size='1x' />
+        </p>{' '}
+      </Link>
+      <button type='submit' className='btnI' onClick={getRandomWord}>
+        Search
+      </button>
     </div>
   );
 };
@@ -67,12 +32,6 @@ dailyLearning.propTypes = {
   prop: PropTypes.func,
 };
 
-const mapStateToProps = (state) => ({
-  search: state.search,
-});
+const mapStateToProps = (state) => ({});
 
-//const mapDispatchToProps = {};
-
-export default connect(mapStateToProps, mapStateToProps, { fetchRandom })(
-  dailyLearning,
-);
+export default connect(mapStateToProps, { fetchRandom })(dailyLearning);
