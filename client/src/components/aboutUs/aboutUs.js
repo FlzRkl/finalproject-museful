3f136209136fac0c33f97770019a49f49f08ff55
filizerkal <erkalfiliz@gmx.net>
2020-07-07 23:30:19 +0200

    About Project

--- client/src/components/aboutUs/aboutUs.js
+++ client/src/components/aboutUs/aboutUs.js
@@ -9,6 +9,21 @@ const iconSize = '4x';
 const aboutUs = () => {
   return (
     <div className='dashboard pro'>
+      <div className='intro'>
+        <h3 className=''>About Museful</h3>
+        <p>
+          Our mission is to help anyone to learn anything they want in a museful
+          and playful way. Let's start to creat a learning and memorizing space
+          which will grow to a user self curated learning management system –
+          welcome to your museful space. Improve your skills with words and
+          terms by getting your <span>Daily Learnings</span> – just for fun.
+          Enjoy your journey by searching for rhyming words or your are curiosed
+          about the meaning of a word, you have the choice to search and filter.
+          If your are seeking for a space to collect and store content you have
+          searched for, so museful is the right place for you.{' '}
+        </p>
+      </div>
+
       <div className='profiles'>
         <a
           class='link aboutUs effectContainer'
@@ -63,12 +78,12 @@ const aboutUs = () => {
         </a>
       </div>
       <div className='intro'>
-        <h1 className=''>About Us</h1>
+        <h3 className=''>About Us</h3>
         <p>
           This is our final project after 8 month of heavy practical web
           development learning at the DCI in Hamburg. We are Filiz, Lana,
-          Harris, Sascha and Stefan and would like to hear from you. You can
-          find our contacts in the links above.
+          Harris, Sascha and Stefan. You can find our contacts in the links
+          above.
         </p>
       </div>
     </div>
